8d2fa3fd93c1fc3edd8524446e5c82a947c6b02c
d4nitrix13 <danielperezdev@proton.me>
2024-08-28 20:53:34 -0600

    update error sintaxis

--- 04 Dockerization/02 App Python Flask/src/app.py
+++ 04 Dockerization/02 App Python Flask/src/app.py
@@ -1,3 +1,7 @@
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielPerezMoralesDev13
+# Correo electrónico: danielperezdev@proton.me
+
 from flask import Flask
 
 app = Flask(__name__)
